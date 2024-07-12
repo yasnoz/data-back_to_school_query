ccbc045b11ca6d499c078c95b80d8df3290a9c9a
Yasin Oz <yasnoz@gmail.com>
2024-07-12 14:18:24 +0200

    5/5 tests passed, return a list of students from a specific city exercise passed successfully

--- school.py
+++ school.py
@@ -1,15 +1,15 @@
 # pylint:disable=C0111,C0103
+import sqlite3
+conn = sqlite3.connect('data/school.sqlite')
+c = conn.cursor()
+
 
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
-
-
-# To test your code, you can **run it** before running `make`
-#   => Uncomment the following lines + run:
-#   $ python school.py
-#
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
-# print(students_from_city(db, 'Paris'))
+    query ="""SELECT s.id, s.first_name, s.last_name
+            FROM students s
+            WHERE s.birth_city = ?
+            """
+    db.execute(query, (city, ))
+    rows = db.fetchall()
+    return rows
